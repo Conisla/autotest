42657364adca30dd45b2cbc81d6251d85cdf00ed
Conisla <nthaothion@gmail.com>
2023-06-20 11:07:27 +0400

    Description et fix update

--- ludtec/api/tests.py
+++ ludtec/api/tests.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from api.models import Jeu
-# Create your tests here.
 
 class JeuAPITest(TestCase):
     def setUp(self):
@@ -36,11 +35,19 @@ class JeuAPITest(TestCase):
         self.assertEqual(response.data['name'], self.jeu.name)
 
     def test_update_jeu(self):
+        # url pour l'update '/api/jeu/<id_jeu>'
         url = reverse('Jeu-detail', args=[self.jeu.id_jeu])
-        data = {'name': 'New Name'}
+        
+        # data à envoyer pour mise a jour 
+        data = {'id_jeu': self.jeu.id_jeu ,'name': 'New Name'}
+        
+        # Envoie de data avec requête HTTP méthode PUT à l'url
         response = self.client.put(url, data, format='json')
 
+        # Test si Status de la requête == HTTP_200
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Test si name de jeu<id_jeu> a été update
         self.assertEqual(Jeu.objects.get(id_jeu=self.jeu.id_jeu).name, 'New Name')
 
     def test_delete_jeu(self):
